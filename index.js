23bec2b4e71db36ab97c79c5a0ea46a7f7dd247d
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2022-11-27 21:17:19 +0000

    written code to create file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "Enter your title for your ReadMe document",
+        message: "Enter your title for your ReadMe document:",
     },
 
     {
@@ -59,23 +59,29 @@ const questions = [
         name: "title",
         message: "Enter your title for your ReadMe document",
     },
-
-
-
-
-
-
 
 
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// This will start the function to create the file
+function writeToFile(fileName, data) {
+    // This will write the data to the "./assets.ReadMe.md file"
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Success!'))
+};
 
 // This function will start the inquirer package to start asking the questions required to complete the ReadMe
-function init() {}
+function init() {
+    inquirer
+    // This will prompt the user the questions
+    .prompt(questions)
+    // This will then take the answers from the user and place it into the generateMarkdown function
+    .then((data) => {
+        const readMeAnswers = generateMarkdown(data);
+        // Then the data is sent to the writeTofile function to create the ReadMe file of the users answers
+        writeToFile("./assets/ReadMe.md", readMeAnswers)
+    });
+};
 
 // Function call to initialize app
 init();
-
-console.log("yo")
